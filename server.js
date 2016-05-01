9d57db72f7d4a75c1b13d16aecef5a183d556200
Abhimanyu Babbar <abhimanyu.babbar@wrapp.com>
2016-05-01 18:17:04 +0200

    Added logic to filter out episodes from movies string.

--- server.js
+++ server.js
@@ -4,10 +4,22 @@ var readline = require('readline');
 function process_movies_list(inLoc, outLoc) {
 
   var start_processing = false;
-  var writable = fs.createWriteStream(outLoc);
+  var writable = fs.createWriteStream(outLoc, {
+    flags: 'w',
+    defaultEncoding: 'utf8',
+    mode: '0o666'
+  });
+
+  writable.on('error', function(error) {
+    console.log(error);
+  });
 
   var linereader = readline.createInterface({
-    input: fs.createReadStream(inLoc)
+    input: fs.createReadStream(inLoc, {
+      flags: 'r',
+      encoding: 'utf8',
+      mode: '0o666'
+    })
   });
 
   linereader.on('line', function(line) {
@@ -31,33 +43,48 @@ function process_movies_list(inLoc, outLoc) {
 
 }
 
-
-function smallTest(inLoc, outLoc) {
-
-  var reader = readline.createInterface({
-    input: fs.createReadStream(inLoc),
-    output: fs.createWriteStream(outLoc)
-  });
-
-  reader.on('line', function(line) {
-    reader.write(line);
-  });
-}
-
-
 function normalize_movies_line(data) {
-  // STAGE 1 : Trim the string field.
+
+  // base: Trim the string field.
   data = data.trim();
 
   if (data === '') {
     return;
   }
 
-  console.log(data);
-  // Level One : Extract the year of the movie by using
-  // split function on the text.
-  pieces = data.split(/\t{1,}/);
-  return pieces;
+  // level one: Extract the year of the movie
+  // by using split function on the text.
+  var row = data.split(/\t{1,}/);
+
+  // stop processing in case we have not
+  // enough elements in the
+  if(row.length < 2){
+    console.log("Not enough arguments.");
+    return;
+  }
+
+  // level two: Remove the ones with no year
+  // or a dash (-) in the year string.
+  var yearStr = row[1];
+  if(yearStr.indexOf('?') != -1 || yearStr.indexOf('-') != -1) {
+    return;
+  }
+
+  // level three: Remove the ones wth sitcom
+  // episode.
+  var episodeStr = row[0];
+  var sitcom = episodeStr.match(/\{(.*)\}/i); // {episode #1} <- remove it.
+
+  if(sitcom !== null) {
+    // need to check for the episode.
+    // strings [{#1.4}, {1992-10-01}] should be avoided.
+    var extract = sitcom[1];
+    if (extract.indexOf('#') != -1 || extract.indexOf('-') != -1) {
+      return;
+    }
+  }
+
+  return row;
 }
 
-process_movies_list('./resources/movies-extract.list', './resources/movies-filtered.list');
+process_movies_list('./resources/movies-extract-even-more.list', './resources/movies-filtered.list');
