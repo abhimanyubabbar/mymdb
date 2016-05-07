0fa8bf9d7a9b40eb150274cff059a93c1b492f2d
Abhimanyu Babbar <abhimanyu.babbar@wrapp.com>
2016-05-07 20:50:02 +0200

    Implemented base version of back pressure in which the system pauses the base stream till application completes operation.

--- util/util.js
+++ util/util.js
@@ -9,6 +9,10 @@
   var file = require('../models/fileProcess');
   var Q = require('q');
 
+  // back-pressure.
+  var bufferCount = 5000;
+  var buffer= [];
+  var dataPushed = false;
 
   /**
    * loadData: Start loading the information
@@ -21,7 +25,7 @@
     var deferred = Q.defer();
     var readStream  = file.createObjectReadStream(location);
 
-    readStream.on('data', _loadDataHandler)
+    readStream.on('data', _dataHandler)
         .on('close', _closeHandler)
         .on('error', _errorHandler);
 
@@ -42,7 +46,56 @@
      * @private
      */
     function _closeHandler(){
-      deferred.resolve('data load complete');
+      console.log('remaining count' + buffer.length);
+      return deferred.resolve('data load complete');
+    }
+
+    /**
+     * handle information.
+     * @param data
+     * @private
+     */
+    function _dataHandler(data){
+
+      // FIX ME : This method is not bullet proof
+      // as it requires entry count to be multiple of bufferCount.
+      // Needs to handle the remaining data when we have
+      if(buffer.length >= bufferCount){
+
+        if (!dataPushed) {
+
+          console.log('buffering complete, going to add new movies now.');
+
+          // pause the stream for
+          // any more data updates.
+          readStream.pause();
+          dataPushed = true;
+
+          console.log('paused the stream now, might get some events in between.');
+
+          // add the information in the
+          // application.
+          db.addMovies(buffer)
+              .then(function(){
+                // once current batch goes through,
+                // be ready to receive and push more data.
+                console.log('add movie batch successful, resuming the stream.');
+                readStream.resume();
+                dataPushed = false;
+              })
+              .catch(function(err){
+                // fail in case any element in
+                // batch fails.
+                console.log('movie batch addition failed, error: ' + err.toString());
+                _errorHandler(err);
+
+              });
+
+          buffer = [];
+        }
+      } else {
+        buffer.push(data);
+      }
     }
 
     /**
